548c1097c3751729722162adf4a8ac7a65b23d94
Novicio-2309 <noviciogaming1997@gmail.com>
2026-03-24 17:37:33 +0800

    Update

--- pb_KM7k.mk
+++ pb_KM7k.mk
@@ -3,10 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
 # Inherit from TECNO-KM7k device
 $(call inherit-product, device/tecno/KM7k/device.mk)
 
