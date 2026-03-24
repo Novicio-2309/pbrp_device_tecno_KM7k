598c03c7b23dcbeebd1cd0753eb2f7a25fd27dae
Novicio-2309 <noviciogaming1997@gmail.com>
2026-03-24 15:07:30 +0800

    Initial commit: PBRP device tree for TECNO SPARK Slim (KM7k)

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/pb_KM7k.mk
+
+COMMON_LUNCH_CHOICES := \
+	pb_KM7k-eng \
+	pb_KM7k-userdebug \
+	pb_KM7k-user
